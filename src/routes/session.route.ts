ee10534bbd1d416c5faf0354b474d71810faaaeb
Jerrol Krause <JerrolKrause@users.noreply.github.com>
2023-08-31 11:24:36 -0400

    Add example of express validation

--- src/routes/session.route.ts
+++ src/routes/session.route.ts
@@ -1,4 +1,10 @@
 import { Router } from 'express';
+import { body, validationResult } from 'express-validator';
+
+export interface Session {
+  username: string;
+  password: string;
+}
 
 const routes = Router();
 /**
@@ -14,6 +20,17 @@ const routes = Router();
  *            example: ["Hello World"]
  */
 routes.get('/session', (_req, res) => {
+  return res.json(['Hello World']);
+});
+
+routes.post('/session', body('username').trim().escape().isLength({ min: 5 }), body('password').trim().escape().isLength({ min: 5 }), (req, res) => {
+  const errors = validationResult(req);
+
+  // Has errors
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ message: 'Validation failed', errors });
+  }
+
   return res.json(['Hello World']);
 });
 
